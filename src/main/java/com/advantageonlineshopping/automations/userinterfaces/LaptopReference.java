bf1d0deb3f1913e1e87041506ee5d28e94d59243
fredy londoño rodriguez <fredy325@gmail.com>
2021-06-16 17:51:36 -0500

    Se cambió el xpath de la referencia del portátil

--- src/main/java/com/advantageonlineshopping/automations/userinterfaces/LaptopReference.java
+++ src/main/java/com/advantageonlineshopping/automations/userinterfaces/LaptopReference.java
@@ -3,5 +3,5 @@ package com.advantageonlineshopping.automations.userinterfaces;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class LaptopReference {
-    public static final Target LAPTOP_REFERENCE = Target.the("Order payment success").locatedBy("(//tr[@id=\"product\"])[2]/td/a/h3");
+    public static final Target LAPTOP_REFERENCE = Target.the("Order payment success").locatedBy("(//h3[@class=\"ng-binding\"])[2]");
 }
